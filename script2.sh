10c5d570a0a39fb6dcfec939115693b06e88710b
drafael-max <drafaelpatin@gmail.com>
2025-12-04 13:19:32 +0000

    Mi script 2

---
+++ script2.sh
@@ -0,0 +1,16 @@
+uname -a 
+lscpu
+free -h
+df -h 
+
+# Muestra toda la información básica del sistema operativo y kernel
+uname -a
+
+# Muestra información detallada del procesador (modelo, núcleos, velocidad, etc.)
+lscpu
+
+# Muestra el uso de memoria RAM y swap en formato legible para humanos
+free -h
+
+# Muestra el espacio usado y disponible en todos los discos/particiones montadas
+df -h
