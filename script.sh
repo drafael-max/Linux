97e0118cbb13fff5a16a0fc906052847cea88561
drafael-max <drafaelpatin@gmail.com>
2025-12-04 12:56:24 +0000

    Mi primer script

---
+++ script.sh
@@ -0,0 +1,21 @@
+#Crear y listas archivos
+echo "Hola Linux" > archivo.txt
+cat archivo.txt
+cp archivo.txt copia.txt
+ls -1
+rm archivo.txt
+
+# Muestra el texto "Hola Linux" y lo guarda en un archivo nuevo llamado archivo.txt
+echo "Hola Linux" > archivo.txt
+
+# Muestra en pantalla el contenido del archivo que acabamos de crear
+cat archivo.txt
+
+# Copia el archivo archivo.txt y crea uno nuevo llamado copia.txt (exactamente igual)
+cp archivo.txt copia.txt
+
+# Lista todos los archivos del directorio actual, uno por línea
+ls -l
+
+# Elimina (borra para siempre) el archivo original llamado archivo.txt
+rm archivo.txt
